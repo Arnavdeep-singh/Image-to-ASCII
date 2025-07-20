f8b502ff23667c5e26fb772bcfd9ccb429b32e19
Arnavdeep Singh <arnavdeep03@gmail.com>
2025-07-20 02:18:53 -0700

    Added characters, convert to grayscale

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from PIL import Image
 # open image
 img = Image.open("/home/arnavdeeps/Desktop/Figure_1.png")
 
+# convert to grayscale
+grayscale = img.convert("L")
+
+CHAR = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,"^`' # reverse this list for brightness inversion
+
+
 # display image
-img.show()
+grayscale.show()
