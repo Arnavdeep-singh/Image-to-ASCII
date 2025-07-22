9b29d61c8180c0a73f7da862850c2875c5416563
Arnavdeep Singh <arnavdeep03@gmail.com>
2025-07-21 22:32:03 -0700

    image processing, mapping brightness

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 from PIL import Image
 
 # open image
-img = Image.open("/home/arnavdeeps/Desktop/Figure_1.png")
+img_url = "/home/arnavdeeps/Desktop/wallpaper.png"
 
-# convert to grayscale
-grayscale = img.convert("L")
+# open image
+img = Image.open(img_url)
 
-CHAR = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,"^`' # reverse this list for brightness inversion
+height = img.height
+width = img.width
+aspect_ratio = height/width
 
+CHAR = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,^`'.-" # reverse this list for brightness inversion
+
+def processImage(image):
+
+    # convert to grayscale
+    grayscale = image.convert("L")
+
+    # resize image
+    resized = image.resize((256,int(round(256*aspect_ratio))))
+
+    return resized
+
+processed = processImage(img)
+
+def getGrayscaleVal(processedImage):
+    brightnessMap = []
+    pixels = processedImage.load()
+    if len(pixels[0,0]) == 3:
+        for x in range(255):
+            for y in range(int(round(256*aspect_ratio))-1):
+                R,G,B = pixels[x,y]
+                brightness = sum([R,G,B])/3
+                brightnessMap.append(int(brightness))
+    elif len(pixels[0,0]) == 4:
+        for x in range(255):
+            for y in range(int(round(256*aspect_ratio))-1):
+                R,G,B,A = pixels[x,y]
+                brightness = sum([R,G,B])/3
+                brightnessMap.append(int(brightness))
+    else:
+        raise("file type incompatible")
+    return brightnessMap
+
+# print(getGrayscaleVal(processed))
 
 # display image
-grayscale.show()
+# processed.show()
+
+print(len(CHAR))
