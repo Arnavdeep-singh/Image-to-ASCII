a0c748a1425177f3681baee7fce640ff0f6cb08e
Arnavdeep Singh <arnavdeep03@gmail.com>
2025-07-28 19:53:39 -0700

    Fixed display issues
    - Works properly now
    - ratio needs to be tweeked

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from PIL import Image
 
 # open image
-img_url = "/home/arnavdeeps/Desktop/wallpaper.png"
+img_url = "/home/arnavdeeps/Desktop/sat.jpg"
+
 
 # open image
 img = Image.open(img_url)
+THIRD = 1/5
 
-height = img.height
-width = img.width
-aspect_ratio = height/width
+og_height = img.height
+og_width = img.width
+aspect_ratio = og_width/og_height
+width = 256
+height = int(width * aspect_ratio * THIRD) 
 
-CHAR = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~i!lI;:,^`'.-" # reverse this list for brightness inversion
-
+CHAR = "$@B%8&WM#oahkbdpqwmZO0QLCJUYXzcvunxjft/\\|()1{}[]?i!lI*^+~;:,-_`'.- " # [::-1] # reverse this list for brightness inversion
+# CHAR = "@$B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 def processImage(image):
 
     # convert to grayscale
-    grayscale = image.convert("L")
+    # grayscale = image.convert("L")
 
     # resize image
-    resized = image.resize((256,int(round(256*aspect_ratio))))
+    resized = image.resize((width,height))
 
     return resized
 
@@ -28,14 +32,14 @@ def getGrayscaleVal(processedImage):
     brightnessMap = []
     pixels = processedImage.load()
     if len(pixels[0,0]) == 3:
-        for x in range(255):
-            for y in range(int(round(256*aspect_ratio))-1):
+        for y in range(height):
+            for x in range(width):
                 R,G,B = pixels[x,y]
                 brightness = sum([R,G,B])/3
                 brightnessMap.append(int(brightness))
     elif len(pixels[0,0]) == 4:
-        for x in range(255):
-            for y in range(int(round(256*aspect_ratio))-1):
+        for y in range(height):
+            for x in range(width):
                 R,G,B,A = pixels[x,y]
                 brightness = sum([R,G,B])/3
                 brightnessMap.append(int(brightness))
@@ -46,19 +50,20 @@ def getGrayscaleVal(processedImage):
 def makeAscii(map):
     ascii = ""
     for n in map:
-        if (len(ascii) % 256 == 0):
+        if ((len(ascii) % (width + 1)) == 0):
             ascii = ascii + "\n"
-        ascii = ascii + CHAR[int(round((n/4)))]
+        ascii = ascii + CHAR[int(round((n/3.87)))]
     return ascii
+
 
 
 with open("demofile.txt", "a") as f:
   f.write(makeAscii(getGrayscaleVal(processed)))
 
 #open and read the file after the appending:
-with open("demofile.txt") as f:
-    print(f.read()) 
-
+# with open("demofile.txt") as f:
+#     print(f.read()) 
+#
 # print(makeAscii(getGrayscaleVal(processed)))
 
 # display image
